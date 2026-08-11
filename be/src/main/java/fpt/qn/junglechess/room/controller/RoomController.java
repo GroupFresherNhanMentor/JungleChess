c2127f0a5374342e026d7b2146128773c9f2a529
Phạm Thế Mạnh <pmanhh19@gmail.com>
2026-08-11 10:57:41 +0700

    Chore/append username as player info (#37)
    * fix fe/be, add username
    
    * merge develop

--- be/src/main/java/fpt/qn/junglechess/room/controller/RoomController.java
+++ be/src/main/java/fpt/qn/junglechess/room/controller/RoomController.java
@@ -71,25 +71,25 @@ public class RoomController {
     public void create(@Payload CreateRoomRequest req, SimpMessageHeaderAccessor sha) {
         String sessionId = sha.getSessionId();
         JwtUserPrincipal principal = extractPrincipal(sha);
-        roomService.createRoom(req, sessionId, principal.userId().toString());
+        roomService.createRoom(req, sessionId, principal.userId().toString(), principal.username());
     }
 
     @MessageMapping("room.{id}.join")
     public void join(@DestinationVariable String id, SimpMessageHeaderAccessor sha) {
         JwtUserPrincipal principal = extractPrincipal(sha);
-        roomService.joinRoom(id, sha.getSessionId(), principal.userId().toString());
+        roomService.joinRoom(id, sha.getSessionId(), principal.userId().toString(), principal.username());
     }
 
     @MessageMapping("room.{id}.rejoin")
     public void rejoin(@DestinationVariable String id, SimpMessageHeaderAccessor sha) {
         JwtUserPrincipal principal = extractPrincipal(sha);
-        roomService.rejoinRoom(id, sha.getSessionId(), principal.userId().toString());
+        roomService.rejoinRoom(id, sha.getSessionId(), principal.userId().toString(), principal.username());
     }
 
     @MessageMapping("room.{id}.watch")
     public void watch(@DestinationVariable String id, SimpMessageHeaderAccessor sha) {
         JwtUserPrincipal principal = extractPrincipal(sha);
-        roomService.watchRoom(id, sha.getSessionId(), principal.userId().toString());
+        roomService.watchRoom(id, sha.getSessionId(), principal.userId().toString(), principal.username());
     }
 
     @MessageMapping("room.{id}.start")
